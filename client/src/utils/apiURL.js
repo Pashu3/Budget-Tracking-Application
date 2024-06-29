f4c6f959ab7f7314bb84b76e94cec69884cf64d2
pashu3 <pashupathimali03@gmail.com>
2024-06-29 17:18:30 +0530

    Client Commit

--- client/src/utils/apiURL.js
+++ client/src/utils/apiURL.js
@@ -1,4 +1,9 @@
-// const url = "https://budget-tracker-backend.onrender.com";
-export const API_URL_USER = "http://localhost:3000/api/v1/users";
-export const API_URL_ACCOUNT = "http://localhost:3000/api/v1/accounts";
-export const API_URL_TRANSACTION = "http://localhost:3000/api/v1/transactions";
+
+//const url = "https://budget-tracking-application-9nup.onrender.com"
+// export const API_URL_USER = "http://localhost:3000/api/v1/users";
+// export const API_URL_ACCOUNT = "http://localhost:3000/api/v1/accounts";
+// export const API_URL_TRANSACTION = "http://localhost:3000/api/v1/transactions";
+
+export const API_URL_USER = "https://budget-tracking-application-9nup.onrender.com/api/v1/users";
+export const API_URL_ACCOUNT = "https://budget-tracking-application-9nup.onrender.com/api/v1/accounts";
+export const API_URL_TRANSACTION = "https://budget-tracking-application-9nup.onrender.com/api/v1/transactions";
